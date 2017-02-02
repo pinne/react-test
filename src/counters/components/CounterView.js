99e0bf13bb27bc01e8bf60cd1f8a02cc3a518c0d
Simon Kers <skers@kth.se>
2017-02-03 00:06:02 +0100

    Turning on PropTypes checking again

--- src/counters/components/CounterView.js
+++ src/counters/components/CounterView.js
@@ -23,4 +23,11 @@ const Value = ({
   )
 }
 
+Value.propTypes = {
+  id: React.PropTypes.number,
+  value: React.PropTypes.number,
+  increment: React.PropTypes.func,
+  decrement: React.PropTypes.func,
+  reset: React.PropTypes.func,
+}
 export default Value
